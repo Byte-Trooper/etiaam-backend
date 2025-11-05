03c1b89f9d3d24a614fad9df23f78de0b27228a4
Byte-Trooper <etiaam.developer@gmail.com>
2025-11-05 13:16:16 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,13 +1,15 @@
-# db.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/app.db")  # <— aquí
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+    connect_args={
+        "ssl": {"ssl_ca": "/etc/ssl/cert.pem"}
+    } if DATABASE_URL.startswith("mysql") else {}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
